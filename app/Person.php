6dd426d67cdbdcf37f42b1c1cce7f81979558e23
Fernando Consalvo <fconsalvo97@gmail.com>
2020-01-26 23:41:30 -0400

    Add email to Person Model

--- app/Person.php
+++ app/Person.php
@@ -9,5 +9,16 @@ class Person extends Model
 
     protected $table = 'person';
     public $timestamps = false;
-    protected $fillable = ['firstName', 'lastName', 'ci', 'phone'];
+    protected $fillable = ['firstName', 'lastName', 'ci', 'phone', 'email'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
 }
