759c8087ba8ad018e9ab2606e54deac8dc6f0f47
gabrielbaron16 <gabrielbu161@gmail.com>
2020-05-29 18:10:09 -0400

    Se Agregó el Endpoint para Responder la Solicitud

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Response as Resp;
+use App\Aid;
+use App\User;
+use App\Request as Req;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class ResponseController extends Controller
 {
@@ -34,7 +40,37 @@ class ResponseController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try{
+            $user = Auth::user();
+            if ($user->role != 'Administrador'){
+                return response()->json(['Message' => 'Unauthorized'], 401);
+            }
+            else
+            {
+                $req = Req::where('id',$request->id_req)->get();
+                Log::channel('stdout')->info($req[0]->id_aid);
+                $aid = Aid::where('id',$req[0]->id_aid)->get();
+                if ($aid[0]->unit <= $request->unit){
+                    return response()->json([
+                        'Error' => 'La Cantidad Asignada Excede al Disponible'], 400);
+                }
+            }
+            $resp = new Resp();
+            $resp->approved = $request->approved;
+            $resp->unit = $request->unit;
+            $resp->id_req = $request->id_req;
+            $resp->created_at = date('Y-m-d');
+            $resp->save();
+            if ($request->approved == true){
+                $aid[0]->unit = $aid[0]->unit - $request->unit;
+                $aid[0]->save();
+            }
+        } catch (Exception $e)
+        {
+            return response()->json([
+                'Error' => 'Error al Registrar Respuesta a Solicitud'], 400);
+        }
+        return $resp->id;
     }
 
     /**
