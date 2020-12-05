a23b8f74badb54e084780e39d10791045e070c66
gabrielbaron16 <gabrielbu161@gmail.com>
2020-12-05 13:32:00 -0400

    Endpint para Crear Usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,9 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
@@ -58,7 +60,50 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userAuth = Auth::user();
+        if ($userAuth->role != 'Administrador') {
+            return response()->json(['Message' => 'Unauthorized'], 401);
+        }
+        $rules = [
+            'name' => 'required|max:20',
+            'lastname' => 'required|max:20',
+            'email' => 'required|email|unique:user',
+            'password' => 'required|min:8',
+            'role' => 'in:Administrador,Solicitante,Consultor'
+        ];
+        $messages = [
+            'name.required' => 'El nombre es requerido',
+            'name.max' => 'El nombre del usuario no puede exceder a :max caracteres',
+            'lastname.required' => 'El apellido es requerido',
+            'lastname.max' => 'El apellido del usuario no puede exceder a :max caracteres',
+            'email.required' => 'El email es requerido',
+            'email.email' => 'El email es inválido',
+            'email.unique' => 'El email ya existe',
+            'password.required' => 'La contraseña es requerida',
+            'password.min' => 'La contraseña debe tener ocho caracteres como mínimo',
+            'role.in' => 'Rol inexistente'
+        ];
+        try
+        {
+            $request->validate($rules,$messages);
+            $user = new User();
+            $user->name = $request->name;
+            $user->lastname = $request->lastname;
+            $user->email = $request->email;
+            $user->role = $request->role;
+            $user->password = Hash::make($request->password);
+            $user->save();
+            $token = $user->createToken('Access Token')->accessToken;
+            return response($user)->header('token', $token)->header('Access-Control-Expose-Headers', 'token');
+        } catch (ValidationException $exception) {
+            Log::channel('stdout')->error($exception->getMessage());
+            return response()->json($exception->validator->errors(),400);
+        } catch (\Exception $exception) {
+            Log::channel('stdout')->error($exception);
+            return response()->json([
+                'Error' => $exception->getMessage()], 400);
+        }
+
     }
 
     /**
